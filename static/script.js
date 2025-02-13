043205bb433fa4e9d3cc633de6d2074ef107ad85
ArGwaster <148948313+ArGwaster@users.noreply.github.com>
2025-02-13 09:12:06 +0100

    better password displaying

--- static/script.js
+++ static/script.js
@@ -7,17 +7,32 @@ document.getElementById('passwordForm').addEventListener('submit', function(even
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById('generatedPassword').innerText = data.password;
+        const passwordElement = document.getElementById('generatedPassword');
+        passwordElement.innerText = '•'.repeat(data.password.length);
+        passwordElement.dataset.password = data.password;
         document.getElementById('result').style.display = 'block';
     })
     .catch(error => console.error('Error:', error));
 });
 
 function copyToClipboard() {
-    const passwordText = document.getElementById('generatedPassword').innerText;
+    const passwordElement = document.getElementById('generatedPassword');
+    const passwordText = passwordElement.dataset.password;
     navigator.clipboard.writeText(passwordText).then(() => {
         alert('Password copied to clipboard!');
     }).catch(err => {
         console.error('Failed to copy password: ', err);
     });
+}
+
+function togglePasswordVisibility() {
+    const passwordElement = document.getElementById('generatedPassword');
+    const toggleButton = document.querySelector('button[onclick="togglePasswordVisibility()"] span');
+    if (passwordElement.innerText.includes('•')) {
+        passwordElement.innerText = passwordElement.dataset.password;
+        toggleButton.innerText = 'visibility_off';
+    } else {
+        passwordElement.innerText = '•'.repeat(passwordElement.dataset.password.length);
+        toggleButton.innerText = 'visibility';
+    }
 }
